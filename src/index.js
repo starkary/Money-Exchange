4b6d8d8f36fcb52ff020e350d798b8fd7ff4e780
Kary <starkary70@gmail.com>
2018-09-30 21:48:58 +0300

    hello

--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function solveEquation(equation) {
+  const solutions = [];
+  let x, x1, x2;
+
+  equation = equation.replace(/\s/g, '');
+  
+    let splitIndices = [];
+  for (let i = equation.length - 1; i >= 0; --i) {
+    if (equation[i] === '+' || equation[i] === '-') {
+      splitIndices.unshift(i);
+    }
+    if (splitIndices.length === 2) {
+      break;
+    }
+  }
+   
+   let substrings = [];
+   
+  substrings.push(equation.slice(0, splitIndices[0]));
+  substrings.push(equation.slice(splitIndices[0], splitIndices[1]));
+  substrings.push(equation.slice(splitIndices[1]));
+  
+  
+  
+  let A = parseInt(substrings[0]),
+   B = parseInt(substrings[1]),
+      C = parseInt(substrings[2]);
+
+
+  
+  let D = (B*B) - 4*A*C; 
+  
+  if (D > 0) {
+   x1 = (-B + Math.sqrt(D))/(2 * A);
+   solutions.push(Math.round(x1));
+   
+   x2 = (-B - Math.sqrt(D) )/(2 * A);
+   solutions.push(Math.round(x2));
+  }
+  
+  else if (D == 0) {
+   x = -B/(2 * A);
+   solutions.push(Math.round(x));
+  }
+  
+   else if (D < 0) {
+   return solutions = [];
+  }
+  
+  return solutions.sort ((a, b) => a - b);
+}
 }
